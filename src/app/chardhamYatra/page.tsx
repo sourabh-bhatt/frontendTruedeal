49e0c857a8d927ec990888722c4a72d167a91ecc
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2025-02-01 15:15:54 +0530

    added the cahrdham

--- src/app/chardhamYatra/page.tsx
+++ src/app/chardhamYatra/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import React from 'react';
 import Link from 'next/link';
 import { FaPlane, FaCalendarAlt, FaClock } from 'react-icons/fa';
 import { Button } from "@/components/ui/button";
